2a0865d212054fbb55f6f73dfb029c55f1ef91fd
Andyy <andy12312345@hotmail.com>
2025-09-18 16:16:39 -0400

    Adding content editting

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ class Task(db.Model):
     def __repr__(self) -> str:
         return f"<Task {self.id}>"
         
-@app.route('/',methods=['POST,GET'])
+@app.route('/',methods=["POST","GET"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         current_task = request.form['content']
         new_task = Task(content=current_task)
         try:
@@ -32,16 +32,39 @@ def index():
         except Exception as e:
             print(f"An error occurred while adding the task: {e}")
             return f"An error occurred while adding the task: {e}"
+    else:
+        tasks = Task.query.order_by(Task.createdAt).all()
+        return render_template('index.html', tasks = tasks)
+    
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = Task.query.get_or_404(id)
+    try: 
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        print(f"An error occurred while deleting the task: {e}")
+        return f"An error occurred while deleting the task: {e}"
+
+@app.route('/update/<int:id>', methods=["POST","GET"])
+def edit(id:int):
+    task=Task.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form['content'] # changes current task content to new content that is in the form
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"An error occurred while updating the task: {e}")
+            return f"An error occurred while updating the task: {e}"
         
-
-
-    return render_template('index.html')
-
-
-
+    else:
+        return "HOME"
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+app.run(debug=True)
